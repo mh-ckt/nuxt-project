27db7a8efdd39a64b78ea79f6a3e35850d03405d
陈可涛 <cacao.chen@heavengifts.com>
2025-08-27 10:24:26 +0800

    feat： 添加nuxt自定义模块的支持

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,21 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: [
+    '@nuxt/ui',
+    [
+      'mh-my-module',
+      {
+        enabled: true,
+        someOption: 'user-value' // 覆盖默认值
+      }
+    ],
+  ],
   css: [
     '@/assets/styles/main.css',
-  ]
+  ],
+  myModule: { // 这个键名对应模块 meta 中的 `configKey`
+    enabled: false,
+    someOption: 'user-value' // 覆盖默认值
+  },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+const { $hello } = useNuxtApp()
 import CaseList from './case-list.vue';
+console.log($hello('World')) 
 </script>
 
 <template>
   <CaseList />
+
 </template>
 
 <style></style>
